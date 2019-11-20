eb5e8e551d0c1b084c99fb80e04be4b8ec879d20
nancy.nali1995@gmail.com <nancy.nali1995@gmail.com>
2019-11-20 11:49:38 -0600

    Work n Progress

--- GroupProject/GroupProject/MainWindow.xaml.cs
+++ GroupProject/GroupProject/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Net.Http;
 using Newtonsoft.Json;
 
+
 namespace GroupProject
 {
     /// <summary>
@@ -24,8 +25,12 @@ namespace GroupProject
     {
         private object convert;
 
+        Results question = new Results();
+        string correctAnswer;
+
         public MainWindow()
         {
+         
             InitializeComponent();
 
             string url = @"https://opentdb.com/api.php?amount=1";
@@ -35,34 +40,88 @@ namespace GroupProject
                 if (response.IsSuccessStatusCode)
                 {
                     var content = response.Content.ReadAsStringAsync().Result;
-                    var question = JsonConvert.DeserializeObject<Results>(content);
+                    question = JsonConvert.DeserializeObject<Results>(content);
 
 
                     foreach (var result in question.results)
                     {
                         questionTB.AppendText(Convert.ToString(result.question));
-                        AnswerLB.Items.Add(Convert.ToString(result.correct_answer));
-                    }
+                        List<string> Answers = new List<string>();
+                        Answers.Add(Convert.ToString(result.correct_answer));
+                        Answers.Add(Convert.ToString(result.incorrect_answers[0]));
+                        Answers.Add(Convert.ToString(result.incorrect_answers[1]));
+                        Answers.Add(Convert.ToString(result.incorrect_answers[2]));
 
-                    foreach (var result in question.results)
-                    {
-                        List<string> incorrect = new List<string>();
-                        incorrect.Add(Convert.ToString(result.incorrect_answer));
-
-                        foreach (var item in incorrect)
+                        correctAnswer = result.correct_answer;
+                        for (int i=0; i< Answers.Count; i++)
                         {
-                            AnswerLB.Items.Add(incorrect);
+                            //Random rnd = new Random();
+                            //int r = rnd.Next(Answers.Count);
+                            AnswerLB.Items.Add(Answers[i]);
+                            //AnswerLB.Items.Add(Answers[1]);
+                            //AnswerLB.Items.Add(Answers[2]);
+                            //AnswerLB.Items.Add(Answers[3]);
                         }
                     }
+                  
+                
+
+                    //foreach (var result in question.results)
+                    //{
+                    //    List<string> incorrect = new List<string>();
+                    //    incorrect.Add(Convert.ToString(result.incorrect_answers[0]));
+                    //    incorrect.Add(Convert.ToString(result.incorrect_answers[1]));
+
+                    //    incorrect.Add(Convert.ToString(result.incorrect_answers[2]));
+
+
+                    //foreach (var item in Answers)
+                    //{
+                    //    AnswerLB.Items.Add(Answers);
+                    //}
+                    ////}
 
                 }
 
 
             }
+         
         }
 
         private void ChooseBt_Click(object sender, RoutedEventArgs e)
         {
+            const int scoreCorrect = 10;
+            int accumulator = 0;
+            int strike = 0;
+            int strikeAccum = 0;
+            int questionsAsked = 0;
+            //using (MainWindow answer = new MainWindow())
+            //    answer.ShowDialog;
+                do
+                {
+                    if (AnswerLB.SelectedItem == correctAnswer)
+                    {
+                        CorrectLB.Items.Add(scoreCorrect + accumulator);
+                        questionsAsked++;
+                    break;
+
+                    }
+                    else
+                    {
+                        StrikeLB.Items.Add(strike + 1);
+                        strikeAccum++;
+                        questionsAsked++;
+                    
+                        if (strikeAccum == 3)
+                        {
+                            MessageBox.Show($"Game Over... you hit three strikes. Your score is {accumulator}!");
+                        }
+                    break;
+                    }
+                } while (questionsAsked < 11 || strike < 3);
+
+            MessageBox.Show($"Great job... you got through all 10 questions. Your end score is {accumulator}.");
+            
             //string url = @"https://opentdb.com/api.php?amount=1";
             //using (HttpClient client = new HttpClient())
             //{
--- GroupProject/GroupProject/Result.cs
+++ GroupProject/GroupProject/Result.cs
@@ -23,7 +23,7 @@ namespace GroupProject
         public string difficulty { get; set; }
         public string question { get; set; }
         public string correct_answer { get; set; }
-        public string[] incorrect_answer { get; set; }
+        public string [] incorrect_answers { get; set; }
 
         //public override string ToString()
         //{
