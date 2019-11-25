86aeeb33933948d08b5de2d7e9b954a4aa1fe8b2
David Allison <davidallison@ou.edu>
2019-11-25 13:17:39 -0600

    cleaned up

--- GroupProject/GroupProject/MainWindow.xaml.cs
+++ GroupProject/GroupProject/MainWindow.xaml.cs
@@ -25,8 +25,6 @@ namespace GroupProject
     {
         private object convert;
 
-
-
         Results question = new Results();
         string correctAnswer;
 
@@ -36,10 +34,8 @@ namespace GroupProject
         const int scoreCorrect = 10;
         int strike = 1;
 
-
         public MainWindow()
         {
-         
             InitializeComponent();
 
             string url = @"https://opentdb.com/api.php?amount=1&type=multiple";
@@ -51,11 +47,11 @@ namespace GroupProject
                     var content = response.Content.ReadAsStringAsync().Result;
                     question = JsonConvert.DeserializeObject<Results>(content);
 
-                    
                     foreach (var result in question.results)
                     {
                         questionTB.AppendText(Convert.ToString(result.question));
                         List<string> Answers = new List<string>();
+
                         Answers.Add(Convert.ToString(result.correct_answer));
                         Answers.Add(Convert.ToString(result.incorrect_answers[0]));
                         Answers.Add(Convert.ToString(result.incorrect_answers[1]));
@@ -74,43 +70,28 @@ namespace GroupProject
                         }
                     }
                 }
-
-
             }
-         
         }
-
-
 
         private void ChooseBt_Click(object sender, RoutedEventArgs e)
         {
-            
-
             do
-                {
+            {
                 if ((string)AnswerLB.SelectedValue == correctAnswer)
-                    {
-                        accumulator = scoreCorrect + accumulator;
-                        CorrectLB.Items.Add(accumulator);
-                        questionsAsked++;
-                        //break;
-
-                    }
-                    else
-                    {
-                        StrikeLB.Items.Add(strike + strikeAccum);
-                        strikeAccum++;
-                        questionsAsked++;
-                    
-                        //if (strikeAccum == 3)
-                        //{
-                        //    MessageBox.Show($"Game Over... you hit three strikes. Your score is {accumulator}!");
-                        //}
-                        //break;
-                    }
+                {
+                    accumulator = scoreCorrect + accumulator;
+                    CorrectLB.Items.Add(accumulator);
+                    questionsAsked++;
+                }
+                else
+                {
+                    StrikeLB.Items.Add(strike + strikeAccum);
+                    strikeAccum++;
+                    questionsAsked++;
+                }
                 
                 questionTB.Text = "";
-                    AnswerLB.Items.Clear();
+                AnswerLB.Items.Clear();
 
                 string url = @"https://opentdb.com/api.php?amount=1&type=multiple";
                 using (HttpClient client = new HttpClient())
@@ -120,7 +101,6 @@ namespace GroupProject
                     {
                         var content = response.Content.ReadAsStringAsync().Result;
                         question = JsonConvert.DeserializeObject<Results>(content);
-
 
                         foreach (var result in question.results)
                         {
@@ -140,11 +120,8 @@ namespace GroupProject
                             }
                         }
                     }
-
-
                 }
                 break;
-
             } while (questionsAsked < 11 || strikeAccum < 3);
 
             if (questionsAsked == 10)
