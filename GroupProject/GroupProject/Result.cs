112023542d7587b89c792baba975327ea522d944
David Allison <davidallison@ou.edu>
2019-11-27 18:32:02 -0600

    11.27

--- GroupProject/GroupProject/Result.cs
+++ GroupProject/GroupProject/Result.cs
@@ -9,17 +9,13 @@ namespace GroupProject
     class Results
     {
         public int response_code { get; set; }
-
         public List<Result> results { get; set; }
-
     }
 
     class Result
     {
         public string category { get; set; }
-
         public string type { get; set; }
-
         public string difficulty { get; set; }
         public string question { get; set; }
         public string correct_answer { get; set; }
